18e8661b4b4c1a911964e503ce3f624dfab79a74
AndreevAV <koriolanrus@gmail.com>
2024-03-22 12:57:21 +0300

    Implemented the operation of menu item 1

--- Task1_Python/Notes.py
+++ Task1_Python/Notes.py
@@ -1,13 +1,36 @@
+import datetime
+
+delimer = '-------------------------------'
+
+def show_notes():
+    with open('notes.csv', 'r', encoding='UTF-8') as file:
+        notes_list = file.read().rstrip().split('\n')
+        if notes_list[0] == '':
+            print('Заметок нет!')
+
+        else:
+            print(delimer)
+            print('Список заметок:\n')
+            for note in notes_list:
+                parse_note(note)
+            print(delimer)
+
+def parse_note(note):
+    parse_note = note.split(';')
+    print('ID: ' + parse_note[0] + '; Дата создания: ' + parse_note[3] + '; Дата изменения: ' + parse_note[3] + '\n'
+          'Тема: ' + parse_note[1] + '\n'
+          'Текст: ' + parse_note[2] + '\n')
+
 def interface():
-    with open('notes.txt', 'a', encoding='UTF-8'):
+    with open('notes.csv', 'a', encoding='UTF-8'):
         pass
     command = '-1'
     while command != '5':
-        print('Возможные варианты взаимодействия:\n'
+        print('Возможные действия:\n'
             '1. Просмотреть заметки\n'
             '2. Добавить заметку\n'
             '3. Редактировать заметку\n'
-            '4. Удлаить заметку\n'
+            '4. Удалить заметку\n'
             '5. Выход из программы')
         
         command = input('Введите пункт меню: ')
@@ -18,7 +41,7 @@ def interface():
 
         match command:
             case '1':   
-                print('1')
+                show_notes()
             case '2':   
                 print('2')                
             case '3':   
@@ -28,3 +51,10 @@ def interface():
             case '5':   
                 print('Всего хорошего!')
 interface()
+
+# идентификатор, заголовок, тело заметки и дату/время создания или последнего изменения заметки.
+
+
+ 
+now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+print(now)
